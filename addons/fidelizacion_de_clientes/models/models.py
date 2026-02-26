5a5d413f79c287244fea28bdd1c461197b1b4218
pierooo <plopezrosas@danielcastelao.org>
2026-02-26 21:53:24 +0100

    TRABAJO 16 FINALIZADO :")

--- addons/fidelizacion_de_clientes/models/models.py
+++ addons/fidelizacion_de_clientes/models/models.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class fidelizacion_de_clientes(models.Model):
-#     _name = 'fidelizacion_de_clientes.fidelizacion_de_clientes'
-#     _description = 'fidelizacion_de_clientes.fidelizacion_de_clientes'
+class fidelizacion_de_clientes(models.Model):
+    _inherit = 'res.partner'
+    Nivel_de_fidelidad = fields.Char(string="Nivel de didelidad",readonly=True,compute="_calcular_fidelizacion", store=True)
+    Codigo_de_socio = fields.Char("Codigo de socio")
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.depends('Codigo_de_socio')
+    def _calcular_fidelizacion(self):
+        for record in self:
+            cods = record.Codigo_de_socio
+
+            if cods:
+                if cods[0] == 'G':
+                    record.Nivel_de_fidelidad = "Gold"
+                else:
+                    record.Nivel_de_fidelidad = "Premium"
+            else:
+                record.Nivel_de_fidelidad = "Estándar"
 
